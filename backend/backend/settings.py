5b496542a2e86bf3c5672d55e1e7c09198e14b05
John Idogun <john@quickcheck.ng>
2022-02-07 11:37:15 +0100

    Initial setup

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # rest
-    'rest_framework_simplejwt.authentication.JWTAuthentication',
 ]
 
 MIDDLEWARE = [
