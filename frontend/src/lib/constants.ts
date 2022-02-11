2ec73143a6b6efc77a48f9e4f439310af1ab3a96
John Idogun <Sirneij@gmail.com>
2022-02-11 11:01:41 +0100

    Update constants.ts

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,1 +1,1 @@
-export const BASE_API_URI = 'http://localhost:8000/api' //'https://django-sveltekit-jwt-auth.herokuapp.com/api'
+export const BASE_API_URI = 'https://django-sveltekit-jwt-auth.herokuapp.com/api'
